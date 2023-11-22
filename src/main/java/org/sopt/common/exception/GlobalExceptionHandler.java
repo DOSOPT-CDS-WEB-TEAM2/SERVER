bd04c4d6b655e3458efe5cbe114cde341bb7c529
sunseo18 <sunseo1818@gmail.com>
2023-11-23 02:54:09 +0900

    [FEAT] Controller 단에서 발생하는 예외를 전역적으로 처리하기 위한 기능 구현 #5

--- src/main/java/org/sopt/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,34 @@
 package org.sopt.common.exception;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final String LOG_FORMAT = "%s";
+    private static final String SERVER_ERROR_MESSAGE = "서버 오류가 발생하였습니다.";
+
+
+    @ExceptionHandler(BaseCustomException.class)
+    public ResponseEntity<ExceptionResponse> handleBaseException(BaseCustomException e) {
+        ExceptionType exceptionType = e.getExceptionType();
+        log.info(String.format(LOG_FORMAT, e.getMessage()), e);
+        return ResponseEntity.status(exceptionType.httpStatus())
+                .body(ExceptionResponse.of(exceptionType.name(), exceptionType.message()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(Exception e) {
+        HttpStatus httpStatus = INTERNAL_SERVER_ERROR;
+        log.error(String.format(LOG_FORMAT, e.getMessage()), e);
+        return ResponseEntity.status(httpStatus)
+                .body(ExceptionResponse.of(httpStatus.name(), SERVER_ERROR_MESSAGE));
+    }
 
 }
