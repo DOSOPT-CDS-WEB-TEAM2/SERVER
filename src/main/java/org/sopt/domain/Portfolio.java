3d485cf8a4871bdfa4f2420c22c256b071ab24a1
sunseo18 <sunseo1818@gmail.com>
2023-11-23 02:46:09 +0900

    [ADD] Stock Entity 구현 #3

--- src/main/java/org/sopt/domain/Portfolio.java
+++ src/main/java/org/sopt/domain/Portfolio.java
@@ -1,5 +1,6 @@
 package org.sopt.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
