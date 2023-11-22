4002678bedb6157d84cc9e68e8a4e3ba6e10c5a5
sunseo18 <sunseo1818@gmail.com>
2023-11-23 02:17:12 +0900

    [ADD] Portfolio Entity 구현 #3

---
+++ src/main/java/org/sopt/domain/Portfolio.java
@@ -0,0 +1,20 @@
+package org.sopt.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
