a3f9abd57f0ee1e87a78eca861ddb512c4146844
sunseo18 <sunseo1818@gmail.com>
2023-11-24 03:36:41 +0900

    [ADD] String으로부터 Continent 객체 찾아서 반환환하는 메서드 구현 #12

--- src/main/java/org/sopt/domain/Continent.java
+++ src/main/java/org/sopt/domain/Continent.java
@@ -1,8 +1,20 @@
 package org.sopt.domain;
 
+import static org.sopt.api.stockIndex.exception.StockIndexExceptionType.NOT_FOUND_CONTINENT;
+
 import lombok.Getter;
+import org.sopt.api.stockIndex.exception.StockIndexException;
 
 @Getter
 public enum Continent {
-    AMERICA, EUROPE, MIDDLE_EAST, AFRICA, ASIA
+    AMERICA, EUROPE, MIDDLE_EAST, AFRICA, ASIA;
+
+    public static Continent find(String continent) {
+        for (Continent c : values()) {
+            if (c.name().equalsIgnoreCase(continent)) {
+                return c;
+            }
+        }
+        throw new StockIndexException(NOT_FOUND_CONTINENT);
+    }
 }
