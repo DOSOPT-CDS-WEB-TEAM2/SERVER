3d6890db4216b872b4988c46b9d0f43290e2f190
mikekks <thdalsrb79@naver.com>
2023-11-26 13:18:04 +0900

    [REFACTOR] 접근자 추가

--- src/main/java/org/sopt/api/stock/controller/dto/response/GetRelatedStockResponseDto.java
+++ src/main/java/org/sopt/api/stock/controller/dto/response/GetRelatedStockResponseDto.java
@@ -1,9 +1,10 @@
 package org.sopt.api.stock.controller.dto.response;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import org.sopt.domain.Stock;
 
-@Builder
+@Builder(access = AccessLevel.PRIVATE)
 public record GetRelatedStockResponseDto(
         String code,
         String name,
