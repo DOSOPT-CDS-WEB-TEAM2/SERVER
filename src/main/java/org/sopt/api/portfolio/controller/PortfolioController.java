d9a03a894db3a1a753e1d71c7014df9a8be36cf1
mikekks <thdalsrb79@naver.com>
2023-11-26 23:30:09 +0900

    [DEL] import문 삭제

--- src/main/java/org/sopt/api/portfolio/controller/PortfolioController.java
+++ src/main/java/org/sopt/api/portfolio/controller/PortfolioController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.sopt.api.portfolio.controller.dto.GetPortfolioDto;
 import org.sopt.api.portfolio.service.PortfolioService;
-import org.sopt.domain.Stock;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
