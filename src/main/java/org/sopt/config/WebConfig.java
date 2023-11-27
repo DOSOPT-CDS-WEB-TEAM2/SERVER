69ff0d60a86dea72905bcaf888622c535429ad8e
mikekks <thdalsrb79@naver.com>
2023-11-27 13:12:09 +0900

    [ADD] WebConfig 추가

--- src/main/java/org/sopt/config/WebConfig.java
+++ src/main/java/org/sopt/config/WebConfig.java
@@ -1,4 +1,13 @@
 package org.sopt.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
