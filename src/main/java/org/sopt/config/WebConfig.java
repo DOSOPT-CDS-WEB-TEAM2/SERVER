941c6ee2682ae51111270589fb5eacb65d4b1e42
sunseo18 <sunseo1818@gmail.com>
2023-11-28 17:18:53 +0900

    [FIX] allowdOrigin -> allowedOriginPatterns로 변경 #29

--- src/main/java/org/sopt/config/WebConfig.java
+++ src/main/java/org/sopt/config/WebConfig.java
@@ -1,13 +1,18 @@
 package org.sopt.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods(HttpMethod.GET.name())
+                .maxAge(3000);
     }
 }
